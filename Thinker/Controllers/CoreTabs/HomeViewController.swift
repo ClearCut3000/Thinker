c2608fec89f008bdf494d405eefbccd07c2542f1
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-03-19 22:41:49 +0300

    Implemented getAllPosts func in DatabaseManager

--- Thinker/Controllers/CoreTabs/HomeViewController.swift
+++ Thinker/Controllers/CoreTabs/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   //MARK: - Properties
   private let composeButton: UIButton = {
@@ -22,12 +22,24 @@ class HomeViewController: UIViewController {
     return button
   }()
 
+  private let tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.register(PostPreviewTableViewCell.self, forCellReuseIdentifier: PostPreviewTableViewCell.identifier )
+    return tableView
+  }()
+
+  private var posts: [BlogPost] = []
+
   //MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+    view.addSubview(tableView)
     view.addSubview(composeButton)
     composeButton.addTarget(self, action: #selector(didTapCreate), for: .touchUpInside)
+    tableView.delegate = self
+    tableView.dataSource = self
+    fetchAllPosts()
   }
 
   //MARK: - Layout
@@ -37,6 +49,7 @@ class HomeViewController: UIViewController {
                                  y: view.frame.height - 88 - view.safeAreaInsets.bottom,
                                  width: 60,
                                  height: 60)
+    tableView.frame = view.bounds
 
   }
 
@@ -46,6 +59,39 @@ class HomeViewController: UIViewController {
     vc.title = "Create Post"
     let navVC = UINavigationController(rootViewController: vc)
     present(navVC, animated: true)
+  }
+
+  private func fetchAllPosts() {
+    DatabaseManager.shared.getAllPosts { [weak self] posts in
+      self?.posts = posts
+      DispatchQueue.main.async {
+        self?.tableView.reloadData()
+      }
+    }
+  }
+
+  //MARK: - TableView Methods
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return posts.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let post = posts[indexPath.row]
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: PostPreviewTableViewCell.identifier, for: indexPath) as? PostPreviewTableViewCell else { fatalError() }
+    cell.configure(with: .init(title: post.title, imageUrl: post.headerImageUrl))
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 100
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    let vc = ViewPostViewController(post: posts[indexPath.row])
+    vc.navigationItem.largeTitleDisplayMode = .never
+    vc.title = "Post"
+    navigationController?.pushViewController(vc, animated: true)
   }
 
 }
